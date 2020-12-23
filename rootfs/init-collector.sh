100d1a0abf7ad360a721f5d0c823dd0b8381f525
Jason Kulatunga <jason@thesparktree.com>
2020-12-22 21:46:27 -0800

    add placeholder collector file so confd works correctly

--- rootfs/init-collector.sh
+++ rootfs/init-collector.sh
@@ -1,5 +1,10 @@
 # Should be kept in sync with services.d/jobber/run
 
+# Create placeholder collector.yaml file if not provided by user
+if [ ! -f "/scrutiny/jobber/jobber.yaml" ]; then
+    touch /scrutiny/config/collector.yaml
+fi
+
 echo "populating jobber config"
 confd -onetime -backend file -file /scrutiny/config/collector.yaml
 
