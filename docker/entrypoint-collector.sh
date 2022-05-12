a004f85145743b2a546f32ee5495d67318d8957c
Jason Kulatunga <jason@thesparktree.com>
2022-05-12 15:24:51 -0700

    fixing cron.
    related #121

--- docker/entrypoint-collector.sh
+++ docker/entrypoint-collector.sh
@@ -7,6 +7,10 @@ printenv | sed 's/^\(.*\)$/export \1/g' > /env.sh
 
 # adding ability to customize the cron schedule.
 COLLECTOR_CRON_SCHEDULE=${COLLECTOR_CRON_SCHEDULE:-"0 0 * * *"}
+
+# if the cron schedule has been overridden via env variable (eg docker-compose) we should make sure to strip quotes
+[[ "${COLLECTOR_CRON_SCHEDULE}" == \"*\" || "${COLLECTOR_CRON_SCHEDULE}" == \'*\' ]] && COLLECTOR_CRON_SCHEDULE="${COLLECTOR_CRON_SCHEDULE:1:-1}"
+
 sed -i 's|{COLLECTOR_CRON_SCHEDULE}|'"${COLLECTOR_CRON_SCHEDULE}"'|g' /etc/cron.d/scrutiny
 
 # now that we have the env start cron in the foreground
