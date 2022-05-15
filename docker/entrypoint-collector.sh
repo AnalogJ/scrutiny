a35c3bae08cac14fadda6b1dc5da438027df86be
Jason Kulatunga <jason@thesparktree.com>
2022-05-14 22:04:46 -0700

    make 50-cron-config and entrypoint-collector.sh mirrors of each other. This is for ease of maintainability.
    related #121

--- docker/entrypoint-collector.sh
+++ docker/entrypoint-collector.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 # Cron runs in its own isolated environment (usually using only /etc/environment )
 # So when the container starts up, we will do a dump of the runtime environment into a .env file that we
-# will then source into the crontab file (/etc/cron.d/scrutiny.sh)
-
-printenv | sed 's/^\(.*\)$/export \1/g' > /env.sh
+# will then source into the crontab file (/etc/cron.d/scrutiny)
+(set -o posix; export -p) > /env.sh
 
 # adding ability to customize the cron schedule.
 COLLECTOR_CRON_SCHEDULE=${COLLECTOR_CRON_SCHEDULE:-"0 0 * * *"}
@@ -11,6 +11,7 @@ COLLECTOR_CRON_SCHEDULE=${COLLECTOR_CRON_SCHEDULE:-"0 0 * * *"}
 # if the cron schedule has been overridden via env variable (eg docker-compose) we should make sure to strip quotes
 [[ "${COLLECTOR_CRON_SCHEDULE}" == \"*\" || "${COLLECTOR_CRON_SCHEDULE}" == \'*\' ]] && COLLECTOR_CRON_SCHEDULE="${COLLECTOR_CRON_SCHEDULE:1:-1}"
 
+# replace placeholder with correct value
 sed -i 's|{COLLECTOR_CRON_SCHEDULE}|'"${COLLECTOR_CRON_SCHEDULE}"'|g' /etc/cron.d/scrutiny
 
 # now that we have the env start cron in the foreground
