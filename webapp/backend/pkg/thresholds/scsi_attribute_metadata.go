fbd5bb57ac0ceaa6aa8f7f6ae120d8b587444a29
Jason Kulatunga <jason@thesparktree.com>
2022-06-09 22:31:35 -0700

    update descriptions for SCSI attributes.

--- webapp/backend/pkg/thresholds/scsi_attribute_metadata.go
+++ webapp/backend/pkg/thresholds/scsi_attribute_metadata.go
@@ -19,7 +19,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "low",
 		Critical:    true,
-		Description: "",
+		Description: "The grown defect count shows the amount of swapped (defective) blocks since the drive was shipped by it's vendor. Each additional defective block increases the count by one.",
 	},
 	"read_errors_corrected_by_eccfast": {
 		ID:          "read_errors_corrected_by_eccfast",
@@ -27,7 +27,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "An error correction was applied to get perfect data (a.k.a. ECC on-the-fly). \"Without substantial delay\" means the correction did not postpone reading of later sectors (e.g. a revolution was not lost). The counter is incremented once for each logical block that requires correction. Two different blocks corrected during the same command are counted as two events.",
 	},
 	"read_errors_corrected_by_eccdelayed": {
 		ID:          "read_errors_corrected_by_eccdelayed",
@@ -35,7 +35,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "An error code or algorithm (e.g. ECC, checksum) is applied in order to get perfect data with substantial delay. \"With possible delay\" means the correction took longer than a sector time so that reading/writing of subsequent sectors was delayed (e.g. a lost revolution). The counter is incremented once for each logical block that requires correction. A block with a double error that is correctable counts as one event and two different blocks corrected during the same command count as two events. ",
 	},
 	"read_errors_corrected_by_rereads_rewrites": {
 		ID:          "read_errors_corrected_by_rereads_rewrites",
@@ -43,7 +43,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "low",
 		Critical:    true,
-		Description: "",
+		Description: "This parameter code specifies the counter counting the number of errors that are corrected by applying retries. This counts errors recovered, not the number of retries. If five retries were required to recover one block of data, the counter increments by one, not five. The counter is incremented once for each logical block that is recovered using retries. If an error is not recoverable while applying retries and is recovered by ECC, it isn't counted by this counter; it will be counted by the counter specified by parameter code 01h - Errors Corrected With Possible Delays. ",
 	},
 	"read_total_errors_corrected": {
 		ID:          "read_total_errors_corrected",
@@ -51,7 +51,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "This counter counts the total of parameter code errors 00h, 01h and 02h (i.e. error corrected by ECC: fast and delayed plus errors corrected by rereads and rewrites). There is no \"double counting\" of data errors among these three counters. The sum of all correctable errors can be reached by adding parameter code 01h and 02h errors, not by using this total.",
 	},
 	"read_correction_algorithm_invocations": {
 		ID:          "read_correction_algorithm_invocations",
@@ -59,7 +59,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "This parameter code specifies the counter that counts the total number of retries, or \"times the retry algorithm is invoked\". If after five attempts a counter 02h type error is recovered, then five is added to this counter. If three retries are required to get stable ECC syndrome before a counter 01h type error is corrected, then those three retries are also counted here. The number of retries applied to unsuccessfully recover an error (counter 06h type error) are also counted by this counter. ",
 	},
 	"read_total_uncorrected_errors": {
 		ID:          "read_total_uncorrected_errors",
@@ -67,7 +67,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "low",
 		Critical:    true,
-		Description: "",
+		Description: "This parameter code specifies the counter that contains the total number of blocks for which an uncorrected data error has occurred. ",
 	},
 	"write_errors_corrected_by_eccfast": {
 		ID:          "write_errors_corrected_by_eccfast",
@@ -75,7 +75,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "An error correction was applied to get perfect data (a.k.a. ECC on-the-fly). \"Without substantial delay\" means the correction did not postpone reading of later sectors (e.g. a revolution was not lost). The counter is incremented once for each logical block that requires correction. Two different blocks corrected during the same command are counted as two events. ",
 	},
 	"write_errors_corrected_by_eccdelayed": {
 		ID:          "write_errors_corrected_by_eccdelayed",
@@ -83,7 +83,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "An error code or algorithm (e.g. ECC, checksum) is applied in order to get perfect data with substantial delay. \"With possible delay\" means the correction took longer than a sector time so that reading/writing of subsequent sectors was delayed (e.g. a lost revolution). The counter is incremented once for each logical block that requires correction. A block with a double error that is correctable counts as one event and two different blocks corrected during the same command count as two events. ",
 	},
 	"write_errors_corrected_by_rereads_rewrites": {
 		ID:          "write_errors_corrected_by_rereads_rewrites",
@@ -91,7 +91,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "low",
 		Critical:    true,
-		Description: "",
+		Description: "This parameter code specifies the counter counting the number of errors that are corrected by applying retries. This counts errors recovered, not the number of retries. If five retries were required to recover one block of data, the counter increments by one, not five. The counter is incremented once for each logical block that is recovered using retries. If an error is not recoverable while applying retries and is recovered by ECC, it isn't counted by this counter; it will be counted by the counter specified by parameter code 01h - Errors Corrected With Possible Delays.",
 	},
 	"write_total_errors_corrected": {
 		ID:          "write_total_errors_corrected",
@@ -99,7 +99,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "This counter counts the total of parameter code errors 00h, 01h and 02h (i.e. error corrected by ECC: fast and delayed plus errors corrected by rereads and rewrites). There is no \"double counting\" of data errors among these three counters. The sum of all correctable errors can be reached by adding parameter code 01h and 02h errors, not by using this total.",
 	},
 	"write_correction_algorithm_invocations": {
 		ID:          "write_correction_algorithm_invocations",
@@ -107,7 +107,7 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "",
 		Critical:    false,
-		Description: "",
+		Description: "This parameter code specifies the counter that counts the total number of retries, or \"times the retry algorithm is invoked\". If after five attempts a counter 02h type error is recovered, then five is added to this counter. If three retries are required to get stable ECC syndrome before a counter 01h type error is corrected, then those three retries are also counted here. The number of retries applied to unsuccessfully recover an error (counter 06h type error) are also counted by this counter. ",
 	},
 	"write_total_uncorrected_errors": {
 		ID:          "write_total_uncorrected_errors",
@@ -115,6 +115,6 @@ var ScsiMetadata = map[string]ScsiAttributeMetadata{
 		DisplayType: "",
 		Ideal:       "low",
 		Critical:    true,
-		Description: "",
+		Description: " This parameter code specifies the counter that contains the total number of blocks for which an uncorrected data error has occurred.",
 	},
 }
