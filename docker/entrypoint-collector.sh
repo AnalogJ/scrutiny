ad4b117f6e2e41626c5e2b945ce965cd43f8a259
Jason Kulatunga <jason@thesparktree.com>
2023-01-11 17:41:09 -0800

    fixes #412
    Added ability to trigger collector at startup.
    Disabled by default, (enable by setting `-e COLLECTOR_RUN_STARTUP=true`)
    
    Added COLLECTOR_RUN_STARTUP_SLEEP env variable to specify seconds before calling scrutiny collector on first run, default sleep value = 1s.

--- docker/entrypoint-collector.sh
+++ docker/entrypoint-collector.sh
@@ -7,12 +7,21 @@
 
 # adding ability to customize the cron schedule.
 COLLECTOR_CRON_SCHEDULE=${COLLECTOR_CRON_SCHEDULE:-"0 0 * * *"}
+COLLECTOR_RUN_STARTUP=${COLLECTOR_RUN_STARTUP:-"false"}
+COLLECTOR_RUN_STARTUP_SLEEP=${COLLECTOR_RUN_STARTUP_SLEEP:-"1"}
 
 # if the cron schedule has been overridden via env variable (eg docker-compose) we should make sure to strip quotes
 [[ "${COLLECTOR_CRON_SCHEDULE}" == \"*\" || "${COLLECTOR_CRON_SCHEDULE}" == \'*\' ]] && COLLECTOR_CRON_SCHEDULE="${COLLECTOR_CRON_SCHEDULE:1:-1}"
 
 # replace placeholder with correct value
 sed -i 's|{COLLECTOR_CRON_SCHEDULE}|'"${COLLECTOR_CRON_SCHEDULE}"'|g' /etc/cron.d/scrutiny
+
+if [[ "${COLLECTOR_RUN_STARTUP}" == "true" ]]; then
+    sleep ${COLLECTOR_RUN_STARTUP_SLEEP}
+    echo "starting scrutiny collector (run-once mode. subsequent calls will be triggered via cron service)"
+    /opt/scrutiny/bin/scrutiny-collector-metrics run
+fi
+
 
 # now that we have the env start cron in the foreground
 echo "starting cron"
