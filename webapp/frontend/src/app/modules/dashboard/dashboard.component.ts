2a987ded8f98505a01b3f71c96493e4846db1de4
Aram Akhavan <github@aram.nubmail.ca>
2026-02-21 20:44:21 -0800

    Make defaut temperature history length 1 week

--- webapp/frontend/src/app/modules/dashboard/dashboard.component.ts
+++ webapp/frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -32,7 +32,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy
     summaryData: { [key: string]: DeviceSummaryModel };
     hostGroups: { [hostId: string]: string[] } = {}
     temperatureOptions: ApexOptions;
-    tempDurationKey = 'forever'
+    tempDurationKey = 'week'
     config: AppConfig;
     showArchived: boolean;
 
