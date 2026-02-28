a87ba4a5df43606283f2253cff42ebab2e252168
Merlin <158784988+merlinz01@users.noreply.github.com>
2026-02-28 11:22:25 -0500

    Set Zulip topic from URL before checking length

--- webapp/backend/pkg/notify/notify.go
+++ webapp/backend/pkg/notify/notify.go
@@ -424,14 +424,16 @@ func (n *Notify) GenShoutrrrNotificationParams(shoutrrrUrl string) (string, *sho
 	case "telegram":
 		(*params)["title"] = subject
 	case "zulip":
+		query := serviceURL.Query()
+		urlTopic := query["topic"]
+		delete(query, "topic")
+		if len(urlTopic) > 0 && urlTopic[len(urlTopic)-1] != "" {
+			subject = urlTopic[len(urlTopic)-1]
+		}
 		subjectRunes := []rune(subject)
 		if len(subjectRunes) > 60 {
 			n.Logger.Warningf("Zulip notification subject too long (%d characters), truncating to 60 characters", len(subjectRunes))
 			subject = string(subjectRunes[:60])
-		}
-		urlTopic := serviceURL.Query()["force_topic"]
-		if len(urlTopic) > 0 && urlTopic[len(urlTopic)-1] != "" {
-			subject = urlTopic[len(urlTopic)-1]
 		}
 		(*params)["topic"] = subject
 	}
